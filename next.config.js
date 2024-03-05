453c2e321069ff0d65f9103b7442b73d7c3c4d4b
James <james@trbl.design>
2024-03-04 22:40:05 -0500

    chore: temp adding mongodb to serverComponentsExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const { withPayload } = require("@payloadcms/next/withPayload");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@payloadcms/db-mongodb"],
+  },
+};
 
 module.exports = withPayload(nextConfig);
