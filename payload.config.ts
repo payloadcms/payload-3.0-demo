1e04a76ecd750e8622f579a9386796ec62178b9b
James <james@trbl.design>
2024-03-07 18:42:59 -0500

    chore: bumps deps

--- payload.config.ts
+++ payload.config.ts
@@ -1,8 +1,8 @@
 import path from "path";
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-// import { slateEditor } from '@payloadcms/richtext-slate'
-// import { mongooseAdapter } from "@payloadcms/db-mongodb"; // database-adapter-import
+// import { postgresAdapter } from '@payloadcms/db-postgres'
+// import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import { mongooseAdapter } from "@payloadcms/db-mongodb"; // database-adapter-import
 import { buildConfig } from "payload/config";
 import sharp from 'sharp'
 import { fileURLToPath } from "url";
@@ -11,8 +11,8 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  editor: lexicalEditor({}),
-  // editor: slateEditor({}),
+  // editor: lexicalEditor({}),
+  editor: slateEditor({}),
   collections: [
     {
       slug: 'pages',
@@ -45,14 +45,14 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.POSTGRES_URI
-    }
-  }),
-  // db: mongooseAdapter({
-  //   url: process.env.MONGODB_URI || '',
+  // db: postgresAdapter({
+  //   pool: {
+  //     connectionString: process.env.POSTGRES_URI || ''
+  //   }
   // }),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URI || '',
+  }),
   async onInit(payload) {
     const existingUsers = await payload.find({
       collection: 'users',
