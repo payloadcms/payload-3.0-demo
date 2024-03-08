8a62afe525fd9997a2df998a14fa134eec8d5d20
James <james@trbl.design>
2024-03-07 19:05:08 -0500

    chore: removes unused code

--- src/app/my-route/route.ts
+++ src/app/my-route/route.ts
@@ -1,9 +1,4 @@
-import config from '@payload-config'
-
 export const GET = async () => {
-  const { editor } = await config;
-  console.log(editor)
-
   return Response.json({
     hello: 'elliot',
   })
