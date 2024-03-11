3594aa90535afc29fd7b7f9a71c7313b5162071e
James <james@trbl.design>
2024-03-10 22:52:41 -0400

    chore: bumps packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,84 +1,8 @@
-/** @type {import('next').NextConfig} */ const withPayload = (nextConfig = {}) => {
-  return {
-    ...nextConfig,
-    experimental: {
-      ...(nextConfig?.experimental || {}),
-      outputFileTracingExcludes: {
-        '**/*': [
-          ...(nextConfig.experimental?.outputFileTracingExcludes?.['**/*'] || []),
-          'drizzle-kit',
-          'drizzle-kit/payload',
-          'libsql',
-        ],
-      },
-      serverComponentsExternalPackages: [
-        ...(nextConfig?.experimental?.serverComponentsExternalPackages || []),
-        'drizzle-kit',
-        'drizzle-kit/payload',
-        'libsql',
-        'pino',
-        'pino-pretty',
-      ],
-    },
-    webpack: (webpackConfig, webpackOptions) => {
-      const incomingWebpackConfig =
-        typeof nextConfig.webpack === 'function'
-          ? nextConfig.webpack(webpackConfig, webpackOptions)
-          : webpackConfig
-      return {
-        ...incomingWebpackConfig,
-        externals: [
-          ...(incomingWebpackConfig?.externals || []),
-          'drizzle-kit',
-          'drizzle-kit/payload',
-          'sharp',
-          'libsql',
-        ],
-        ignoreWarnings: [
-          ...(incomingWebpackConfig?.ignoreWarnings || []),
-          {
-            module: /node_modules\/mongodb\/lib\/utils\.js/,
-          },
-          {
-            file: /node_modules\/mongodb\/lib\/utils\.js/,
-          },
-          {
-            module: /node_modules\/mongodb\/lib\/bson\.js/,
-          },
-          {
-            file: /node_modules\/mongodb\/lib\/bson\.js/,
-          },
-        ],
-        resolve: {
-          ...(incomingWebpackConfig?.resolve || {}),
-          alias: {
-            ...(incomingWebpackConfig?.resolve?.alias || {}),
-          },
-          fallback: {
-            ...(incomingWebpackConfig?.resolve?.fallback || {}),
-            '@aws-sdk/credential-providers': false,
-            '@mongodb-js/zstd': false,
-            aws4: false,
-            kerberos: false,
-            'mongodb-client-encryption': false,
-            snappy: false,
-            'supports-color': false,
-            'yocto-queue': false,
-          },
-        },
-      }
-    },
-  }
-}
-//# sourceMappingURL=withPayload.js.map
+import withPayload from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   outputFileTracingIncludes: {
-  //     pino: ['./node_modules/pino'],
-  //   },
-  // },
+  // Your Next.js config here
 }
 
 export default withPayload(nextConfig)
