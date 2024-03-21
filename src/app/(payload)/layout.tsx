74c4ad41f0ce978ae86028883213aadedaed816c
Elliot DeNolf <denolfe@gmail.com>
2024-03-21 17:31:27 -0400

    feat: alpha updates

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -2,7 +2,7 @@
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import React from 'react'
 import configPromise from '@payload-config'
-import { RootLayout } from '@payloadcms/next/layouts/Root/index'
+import { RootLayout } from '@payloadcms/next/layouts'
 
 import '@payloadcms/next/css'
 import './custom.scss'
