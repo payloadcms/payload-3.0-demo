a9e525719424b23e293fb72e7eb6b6e35c0134eb
Tylan Davis <89618855+tylandavis@users.noreply.github.com>
2024-04-15 16:19:08 -0400

    chore: restyles start page (#114)
    * restyles start page
    
    * chore: start page style tweaks
    
    * chore: adjust codeblock overflow
    
    * chore: satisfies the design gods

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './globals.scss'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 /* Our app sits here to not cause any conflicts with payload's root layout  */
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <html>
-      <body>
-        <main>{children}</main>
-      </body>
+    <html className={inter.className}>
+      <body>{children}</body>
     </html>
   )
 }
